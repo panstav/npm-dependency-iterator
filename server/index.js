ed2a16d44647b28a61e324b60c9195b22feba1a8
Stav Pan Geffen <stavgeffen@gmail.com>
2016-06-16 13:10:21 +0300

    Prettyfied json output

--- server/index.js
+++ server/index.js
@@ -8,6 +8,9 @@ function initServer(){
 
 	const server = express();
 
+	// prettify response json
+	server.set('json spaces', 4);
+
 	server.get('/cache', routes.cache);
 	server.get('/deps/:name', routes.deps);
 	server.get('/tree/:name', routes.tree);
